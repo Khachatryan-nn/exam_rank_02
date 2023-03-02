c0f553b53a82b45d39975dcf7bf8552cc380e257
Khachatryan-nn <tigrankh02@gmail.com>
2023-03-02 15:19:15 +0400

    one more commit

--- Rank_1/first_word.c
+++ Rank_1/first_word.c
@@ -17,15 +17,15 @@ int	main(int argv, char **argc)
 	int	i;
 
 	i = 0;
-	if (!argc[1])
-		return (0);
-	while (argc[1][i] == ' ' || (argc[1][i] >= 9 && argc[1][i] <= 13))
-		i++;
-	while (argc[1][i] != '\0' && argc[1][i] != ' ' && \
-			(argc[1][i] <= 9 || argc[1][i] >= 13))
+	if (argv == 2)
 	{
-		write(1, &argc[1][i], 1);
-		i++;
+		while (argc[1][i] == 32 || argc[1][i] == 9)
+			i++;
+		while (argc[1][i] && argc[1][i] != 9 && argc[1][i] != 32)
+		{
+			write(1, &argc[1][i], 1);
+			i++;
+		}
 	}
 	write (1, "\n", 1);
 	return (0);
