41cda8acc9b03259ac81498bc77305663b26dba0
Tigran Khachatryan <tikhacha@c2r3s6.42yerevan.am>
2023-03-07 17:39:18 +0400

    new commit

---
+++ Rank_1/ft_ulstr.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int	main(int argv, char **argc)
+{
+	int	i;
+
+	i = 0;
+	if (argv == 2)
+	{
+		while (argc[1][i])
+		{
+			if (argc[1][i] >= 'a' && argc[1][i] <= 'z')
+				argc[1][i] -= 32;
+			else if (argc[1][i] >= 'A' && argc[1][i] <= 'Z')
+				argc[1][i] += 32;
+			write (1, &argc[1][i++], 1);
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
