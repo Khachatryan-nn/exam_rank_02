82b274fec57ff6335d3b316cefc1e1dc0b3f23e8
Tigran Khachatryan <tikhacha@c2r3s6.42yerevan.am>
2023-03-22 18:59:36 +0400

    new comm

---
+++ lvl_3/ft_list_size/ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tikhacha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 18:53:11 by tikhacha          #+#    #+#             */
+/*   Updated: 2023/03/22 18:55:01 by tikhacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+int	ft_list_size(t_list *begin_list);
+
+#endif
---
+++ lvl_3/ft_list_size/ft_list_size.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tikhacha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 18:55:12 by tikhacha          #+#    #+#             */
+/*   Updated: 2023/03/22 18:59:08 by tikhacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
+	t_list	*temp;
+
+	i = 0;
+	temp = begin_list;
+	while (temp->next != NULL)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
+
+int	main(void)
+{
+
