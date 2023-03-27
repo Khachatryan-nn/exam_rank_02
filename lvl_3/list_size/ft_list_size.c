b3d1b5520aaa52f1d381e8552d04e09d9a2ef397
Tigran Khachatryan <tikhacha@c2r2s4.42yerevan.am>
2023-03-27 20:29:31 +0400

    lists

--- lvl_3/list_size/ft_list_size.c
+++ lvl_3/list_size/ft_list_size.c
@@ -6,11 +6,13 @@
 /*   By: tikhacha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:55:12 by tikhacha          #+#    #+#             */
-/*   Updated: 2023/03/22 19:27:25 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/03/27 16:55:35 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "ft_list.h"
 
 int	ft_list_size(t_list *begin_list)
 {
@@ -19,7 +21,7 @@ int	ft_list_size(t_list *begin_list)
 
 	i = 0;
 	temp = begin_list;
-	while (temp->next != NULL)
+	while (temp != NULL)
 	{
 		i++;
 		temp = temp->next;
@@ -27,6 +29,27 @@ int	ft_list_size(t_list *begin_list)
 	return (i);
 }
 
-int	main(void)
-{
+int main(void) {
+    // create a linked list
+    t_list *head = malloc(sizeof(t_list));
+    t_list *second = malloc(sizeof(t_list));
+    t_list *third = malloc(sizeof(t_list));
 
+    head->data = 0;
+    head->next = second;
+    second->data = 0;
+    second->next = third;
+    third->data = 0;
+    third->next = NULL;
+
+    // test the ft_list_size function
+    int size = ft_list_size(head);
+    printf("The size of the linked list is: %d\n", size);
+
+    // free memory
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
