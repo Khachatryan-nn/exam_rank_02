40600b5cec235802a9041410e9ffbda63478b3d7
Khachatryan-nn <tigrankh02@gmail.com>
2023-03-28 08:24:31 +0400

    commit 3

--- lvl_4/ft_fprime.c
+++ lvl_4/ft_fprime.c
@@ -3,98 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fprime.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tikhacha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tikhacha <tikhacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:04:12 by tikhacha          #+#    #+#             */
-/*   Updated: 2023/03/27 20:25:02 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/03/28 01:18:05 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_isprime(int	num)
+void	ft_finder(int num, int pnmb)
 {
 	int	i;
-
-	i = 1;
-	while (++i < num)
-	{
-		if (num % i == 0)
-			return (0);
-	}
-	return (1);
-}
-
-int	ft_counter(int	num)
-{
-	int	i;
-	int	j;
 
 	i = 0;
-	j = 2;
 	while (num > 1)
 	{
-		if (num % j == 0)
+		if (num % pnmb == 0)
 		{
+			if (i > 0)
+				printf ("*%d", pnmb);
+			else
+				printf ("%d", pnmb);
+			num /= pnmb;
 			i++;
-			num /= j;
 		}
 		else
-		{
-			while (!ft_isprime(++j))
-				;
-		}
+			pnmb++;
 	}
-	return (i);
-}
-
-int	*ft_set_array(int num, int len)
-{
-	int	i;
-	int	j;
-	int	*x;
-
-	i = 0;
-	j = 2;
-	x = (int *)malloc(sizeof(int) * len);
-	while (num > 1)
-	{
-		if (num % j == 0)
-		{
-			x[i++] = j;
-			num /= j;
-		}
-		else
-		{
-			while (!ft_isprime(++j))
-				;
-		}
-	}
-	return (x);
 }
 
 int	main(int argv, char **argc)
 {
-	int	i;
-	int	len;
+	int	prime_num;
 	int	num;
-	int	*array;
 
-	i = 0;
-	len = 0;
-	num = 0;
+	prime_num = 2;
 	if (argv == 2)
 	{
 		num = atoi(argc[1]);
-		len = ft_counter(num);
-		array = ft_set_array(num, len);
-		while (i < len)
+		if (num == 1)
+			printf ("1\n");
+		else
 		{
-			if (i > 0)
-				printf ("*");
-			printf("%d", array[i++]);
+			ft_finder(num, prime_num);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 }
